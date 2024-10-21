1a5b83c5523a42e14c5cc4d813fcbb44efefe2ee
BlackR-B <nzn.gammer@gmail.com>
2024-10-21 23:49:50 +0700

    rgdg

--- Test/burgur.py
+++
@@ -1,9 +0,0 @@
-'''AB'''
-def main():
-    '''sum'''
-    A = int(input())
-    B = int(input())
-    C = (A + B) * 2
-    burgur = "|"* A + "*" * C + "|" * B
-    print(burgur)
-main()
--- Test/คลี่น.py
+++
@@ -1,8 +0,0 @@
-'''LOL'''
-def main():
-    '''คลี่น'''
-    k = int(input())
-    n = int(input())
-    for i in range(n):
-        print(" " * abs(n // 2 - i) + "*" * k)
-main()
